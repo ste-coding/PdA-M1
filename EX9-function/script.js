ecc5ff8a9f2964d4a04783a6d107afc3b9ad9443
Stéphanie Cândido <stephanie.candido42@gmail.com>
2024-02-08 10:54:30 -0300

    refactor: remove unnecessary comments from code

--- EX9-function/script.js
+++ EX9-function/script.js
@@ -1,9 +1,3 @@
-/*Crie uma função que cumprimente o usuário pelo nome que ele informar utilizando um alert. Para isso, utilize a função prompt para pedir que o usuário, o armazene em uma variável e informe o nome juntamente com o cumprimento, a mensagem de cumprimento deve ser do tipo “Olá, {nome informado}!”.*/
-
-/*crie 4 funcoes representando as 4 operacoes matematicas
-// onde cada funcao vai pegar 2 valores e calcular baseado 
-// na operacao definida pelo nome da funcao */
-
 function username() {
     let name = prompt("Informe o seu nome:");
     alert(`Seja bem-vindo(a), ${name} !`);
